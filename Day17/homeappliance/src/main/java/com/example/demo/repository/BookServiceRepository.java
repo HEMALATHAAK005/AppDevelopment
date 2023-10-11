1f175732116a9d2e7c0500211f23b926474f0b7f
HEMALATHAAK005 <110532264+HEMALATHAAK005@users.noreply.github.com>
2023-10-11 09:28:04 +0530

    Add files via upload

---
+++ Day17/homeappliance/src/main/java/com/example/demo/repository/BookServiceRepository.java
@@ -0,0 +1,17 @@
+
+package com.example.demo.repository;
+
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.BookService;
+
+
+
+
+
+public interface  BookServiceRepository extends JpaRepository <BookService,Integer>{
+
+}
